04d3fca20734fa0a54a3d64b7063b473de335423
chappelle <cchhppppllll@gmail.com>
2020-09-15 12:06:04 +0200

    aaaaaand its back

---
+++ get_next_line/osx/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouchau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/20 12:45:41 by abouchau          #+#    #+#             */
+/*   Updated: 2020/01/30 15:51:11 by abouchau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <limits.h>
+
+int		check_new_line(char *s);
+int		get_next_line(int fd, char **line);
+
+int		ft_strlen(const char *str);
+char	*ft_substr(char const *s, int start, int len);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *src);
+#endif
